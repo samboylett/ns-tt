3c8158aa162142d811808229155467dc9e6db178
Sam Boylett <sam.boylett@piksel.com>
2018-08-20 09:52:32 +0100

    eject and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow((
+      <App />
+    ));
+  });
+
+  it('renders without crashing', () => {
+    expect(component).toBeDefined();
+  });
 });
